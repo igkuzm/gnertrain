527aa664d828412cf2ac052a6e9de813d3530080
Igor V. Sementsov <ig.kuzm@gmail.com>
2022-11-14 19:39:19 +1000

    fix

--- src/gnerdtrain.h
+++
@@ -1,9 +0,0 @@
-/**
- * File              : gnerdtrain.h
- * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
- * Date              : 12.11.2022
- * Last Modified Date: 12.11.2022
- * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
- */
-
-#include <stdio.h>
